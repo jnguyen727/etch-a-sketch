11af60214a6ef877dc376f74b14187ce67e21b83
jnguyen727 <styliss777@gmail.com>
2023-12-10 11:25:52 -0500

    Functioning etch-a-sketch.

--- script.js
+++ script.js
@@ -1,53 +1,31 @@
+let dimensions = 16;
+
+
 const bigContainer = document.createElement('div');
 bigContainer.style.display = 'flex';
-bigContainer.style.flexDirection = 'row';
+bigContainer.style.flexDirection = 'column';
 
-
-const containerDiv = document.createElement('div');
-containerDiv.style.display = 'flex';
-containerDiv.style.flexDirection = 'row';
-
-
-
-const containerDiv2 = document.createElement('div');
-containerDiv2.style.display = 'flex';
-containerDiv2.style.flexDirection = 'row';
-
-
-
-for (let i = 0; i < 16; i++) {
-        const gridDiv = document.createElement('div');
-        gridDiv.style.border = '1px solid black';
-        gridDiv.style.width = '100%';
-        gridDiv.style.height = '6.25vh';
-
-
-        containerDiv.appendChild(gridDiv);
-
-}
-
-for (let i = 0; i < 16; i++) {
-    const gridDiv = document.createElement('div');
-    gridDiv.style.border = '1px solid black';
-    gridDiv.style.width = '100%';
-    gridDiv.style.height = '6.25vh';
-
-
-    containerDiv2.appendChild(gridDiv);
-
-}
-
-
-
-document.body.appendChild(containerDiv);
-document.body.appendChild(containerDiv2);
 document.body.appendChild(bigContainer);
 
+for (let i = 0; i < dimensions; i++) {
+    const containerDiv = document.createElement('div');
+    containerDiv.style.display = 'flex';
+    containerDiv.style.flexDirection = 'row';
 
+    for (let j = 0; j < dimensions; j++) {
+        const gridDiv = document.createElement('div');
+        gridDiv.style.border = '1px solid black';
+        gridDiv.style.width = '6.25%';
+        gridDiv.style.height = '6.25vh';
+        containerDiv.appendChild(gridDiv);
 
+        gridDiv.addEventListener('mouseover', () => {
+            gridDiv.style.backgroundColor = 'black';
+        });
 
+        
+    }
 
-
-
-
+    bigContainer.appendChild(containerDiv);
+}
 
