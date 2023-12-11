86fb9a84ba96bb45000d4581ab1149b401a42bd2
jnguyen727 <styliss777@gmail.com>
2023-12-11 11:57:27 -0500

    Add functioning button that removes current grid and adheres to the screen.

--- script.js
+++ script.js
@@ -1,45 +1,60 @@
 let dimensions = 16;
 
+// Create header element
+const header = document.createElement('header');
+document.body.appendChild(header);
 
+// Create button and append it to the header
+const button = document.createElement('button');
+button.textContent = 'hey';
+header.appendChild(button);
+
+// Create grid container
 const bigContainer = document.createElement('div');
 bigContainer.style.display = 'flex';
 bigContainer.style.flexDirection = 'column';
-
-const button = document.createElement('button');
-button.textContent = 'hey';
-button.addEventListener('click', () => {
-    let newDimension = prompt("What dimensions do you want the grid to be?")
-    dimensions = newDimension;
-
-    
-    
-})
-document.body.appendChild(button);
-
 document.body.appendChild(bigContainer);
 
-for (let i = 0; i < dimensions; i++) {
-    const containerDiv = document.createElement('div');
-    containerDiv.style.display = 'flex';
-    containerDiv.style.flexDirection = 'row';
+// Function to create the grid
+function createGrid() {
+    let objectDimensions = (100 / dimensions);
+    for (let i = 0; i < dimensions; i++) {
+        const containerDiv = document.createElement('div');
+        containerDiv.style.display = 'flex';
+        containerDiv.style.flexDirection = 'row';
 
-    for (let j = 0; j < dimensions; j++) {
-        const gridDiv = document.createElement('div');
-        gridDiv.style.border = '1px solid black';
-        gridDiv.style.width = '6.25%';
-        gridDiv.style.height = '6.25vh';
-        containerDiv.appendChild(gridDiv);
+        for (let j = 0; j < dimensions; j++) {
+            const gridDiv = document.createElement('div');
+            gridDiv.style.border = '1px solid black';
+            gridDiv.style.width = objectDimensions + '%';
+            gridDiv.style.height = objectDimensions + 'vh';
+            containerDiv.appendChild(gridDiv);
 
-        gridDiv.addEventListener('mouseover', () => {
-            gridDiv.style.backgroundColor = 'black';
-        });
+            gridDiv.addEventListener('mouseover', () => {
+                gridDiv.style.backgroundColor = 'black';
+            });
+        }
 
-        
+        bigContainer.appendChild(containerDiv);
     }
-
-    bigContainer.appendChild(containerDiv);
 }
 
+// Initial grid creation
+createGrid();
 
+// Button click event
+button.addEventListener('click', () => {
+    let newDimension = prompt("What dimensions do you want the grid to be?");
+    if (newDimension < 101) {
+        dimensions = newDimension
+    } else {
+        alert("Please pick a value that is 100 or less!");
+    }
+    console.log(dimensions);
 
+    // Clear the grid container
+    bigContainer.innerHTML = '';
 
+    // Recreate the grid
+    createGrid();
+});
